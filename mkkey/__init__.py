2f2b0769665bb6a0e62d0bc02c611c8b6e04f9e3
Ajitomi Daisuke <dajiaji@gmail.com>
2021-12-11 18:02:04 +0900

    Refine title.

--- mkkey/__init__.py
+++ mkkey/__init__.py
@@ -1,8 +1,8 @@
 from .cli import cli
 
 __version__ = "0.6.0"
-__description__ = "A Generic Application-Layer Key Generator as a CLI Application."
-__url__ = "https://mkkey.readthedocs.io"
+__description__ = "Application-Layer Key (JWK/PASERK) Generator"
+__url__ = "https://github.com/dajiaji/mkkey"
 __uri__ = __url__
 __doc__ = __description__ + " <" + __uri__ + ">"
 __copyright__ = "Copyright 2021 Ajitomi Daisuke"
