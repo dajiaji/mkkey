98cb60a78d3b53616a5fea751d4829e06d6ed534
Ajitomi Daisuke <dajiaji@gmail.com>
2023-09-17 11:18:12 +0900

    Bump version up to 0.7.0.

--- mkkey/__init__.py
+++ mkkey/__init__.py
@@ -1,7 +1,7 @@
 from .cli import cli
 
-__version__ = "0.6.2"
-__description__ = "Application-Layer Key (JWK/PASERK) Generator"
+__version__ = "0.7.0"
+__description__ = "An Application-Layer Key (JWK/PASERK) Generator"
 __url__ = "https://github.com/dajiaji/mkkey"
 __uri__ = __url__
 __doc__ = __description__ + " <" + __uri__ + ">"
